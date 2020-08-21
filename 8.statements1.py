806f70d8f7ea189aa79ed509030586faa5bc3d46
JunYoungYoon <jjun9288@gmail.com>
2020-08-21 17:08:34 +0900

    week_8

---
+++ 8.statements1.py
@@ -0,0 +1,17 @@
+# statements1
+# 제어문
+#   조건문, 반복문
+# 조건문
+# ex) 성적이 90점 이상이면, A라고 출력하고
+#     성적이 80점 이상이면, B라고 출력하세요.
+grade = int(input("성적 입력 : "))
+
+if grade >= 90:
+    print ('A')
+elif grade >= 80:
+    print ('B')
+elif grade >= 80:
+    print ('C')
+elif grade >= 80:
+    print ('D')
+else : print ('F')
